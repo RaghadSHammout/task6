f122cfb03c762b6a660003354286c825242db934
RaghadSHammout <raghadshammout3010@gmail.com>
2024-10-29 22:55:41 +0300

    first commit

---
+++ assets/js/bestSellingBook.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const apiUrl = "https://wolnelektury.pl/api/books/studnia-i-wahadlo/";
+
+    fetch(apiUrl)
+        .then(response => {
+           
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            document.getElementById("book-image").src = data.cover || ''; 
+            document.getElementById("title").textContent = data.title || 'No title available';
+
+            const authorName = data.authors && data.authors.length > 0 ? data.authors[0].name : "Unknown Author";
+            document.getElementById("author").textContent = `By ${authorName}`;
+
+        })
+        .catch(error => {
+            console.error("Error fetching book data:", error);
+        });
+});
---
+++ assets/js/featuredBooks.js
@@ -0,0 +1,31 @@
+async function fetchFeaturedBooks() {
+    
+    try {
+        const response = await fetch('https://wolnelektury.pl/api/authors/adam-mickiewicz/kinds/liryka/parent_books/');
+        const data = await response.json();
+        const featuredBooks = data.slice(-4);
+        const booksRow = document.getElementById('featured-books-row');
+
+        featuredBooks.forEach(book => {
+            const card = `
+                <div class="col-md-3 mb-4 d-flex justify-content-center mt-5">
+                    <div class="card border-0 text-center book-card">
+                        <div class="d-flex justify-content-center align-items-center book-bg">
+                            <img src="${book.simple_thumb}" class="rounded-3 shadow-sm book-img" alt="${book.title}">
+                        </div>
+                        <div class="card-body bg-light-color">
+                            <h5 class="book-title text-center mb-2">${book.title}</h5>
+                            <p class="book-author text-muted mb-2">${book.author}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+            booksRow.innerHTML += card;
+        });
+        
+    } catch (error) {
+        console.error('Error fetching featured books:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', fetchFeaturedBooks);
